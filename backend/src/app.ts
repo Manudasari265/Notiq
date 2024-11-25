4708e93a08135e25e553948e379132d36c8c5f62
Manoj Dasari <dasarimanoj265@gmail.com>
2024-11-25 23:27:50 +0530

    feat: signup route init

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,13 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
+app.post("/api/v1/signup", function(req: Request, res: Response) {
+    const { firstName, lastName } = req.body;
+    const { username } = req.body;
+    const { password } = req.body;
+    const { email } = req.body;
 
+    //TODO hash the password, input validation using zod
+})
 
 export default app;

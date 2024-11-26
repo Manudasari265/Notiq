44325ad40b3416b9466db9b6d4863004d339d6ee
Manoj Dasari <dasarimanoj265@gmail.com>
2024-11-26 22:48:17 +0530

    update: changed the error and success response codes

--- backend/src/types/ResponseStatus/ResponseErrors.types.ts
+++ backend/src/types/ResponseStatus/ResponseErrors.types.ts
@@ -1,9 +1,9 @@
 export const ResponseErrors = {
     BAD_REQUEST: 400,
-    UNAUTHORIZED: 401,
     FORBIDDEN: 403,
     NOT_FOUND: 404,
     INPUT_ERRORS: 411,
+    DB_DUPLICATE_KEY: 11000,
 } as const
 
 export type ResponseErrorCode = typeof ResponseErrors[keyof typeof ResponseErrors];
